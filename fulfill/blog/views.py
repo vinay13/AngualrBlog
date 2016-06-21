3f182549eed09e0cfb2965bde5863000b7e3b746
vinay singh <aitvinay@gmail.com>
2016-06-21 19:39:52 +0530

    Update views.py

--- fulfill/blog/views.py
+++ fulfill/blog/views.py
@@ -1,23 +1,16 @@
-from rest_framework import viewsets,filters
+from rest_framework import viewsets
 from rest_framework.response import Response
 from blog.models import Blog
 from blog.serializers import BlogSerializer
-from django.shortcuts import render_to_response, get_object_or_404
 
-"""
-def index(request):
-    return render_to_response('blog/index.html', {
-        #'blog': get_object_or_404(Blog)
-    })
 
-"""
+
 
 
 class BlogViewSet(viewsets.ModelViewSet):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
-#    filter_backends = (filters.DjangoFilterBackend,)
-#    filter_fields = ('email',)
+
 
 
 
